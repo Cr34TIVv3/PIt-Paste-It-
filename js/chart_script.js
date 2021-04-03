df7c4d15206be281473c7c6adf946dbabaddd5e5
Iustin Petrariu <57269343+IustinianPetrariu@users.noreply.github.com>
2021-04-03 22:20:18 +0300

    improvement on: facing,account,charts

--- js/chart_script.js
+++ js/chart_script.js
@@ -1,7 +1,14 @@
-window.onload = function() {
-    console.log("Asd")
-    e1 = new Element(" Users", "#417333", "75");
-    e2 = new Element(" Pastes", "#569944", "100");
+function start() {
+    navbarEvent();
+    chartGenerator();
+}
+
+window.onload = start;
+
+
+function chartGenerator() {
+    e1 = new Element(" Public Pastes", "#417333", "75");
+    e2 = new Element(" Private Pastes", "#569944", "100");
     e3 = new Element(" Visitors", "#3B692E", "40");
     elementList = Array();
     elementList.push(e1);
@@ -25,20 +32,22 @@ function createChart(marginBot, marginTop, listElements, rectWidth, spacing) {
 
 
     var maxHeight = height - marginTop - marginBot; 
+    console.log(maxHeight);
 
     for(var i=0; i< listElements.length; i++) {
         var percentage = (listElements[i].value * 100) / maxElement.value;
         var currHeight = (percentage * maxHeight) / 100;
 
-        can.fillStyle = "black";
+        can.fillStyle = "white";
         can.font = "bold 16px Arial";
         can.fillText(listElements[i].value, x, y + (maxHeight - currHeight) - 2);
 
         can.fillStyle = listElements[i].color;
         can.fillRect(x, y + (maxHeight - currHeight), rectWidth, maxHeight - ( y + (maxHeight - currHeight)) );
-        can.fillStyle = "black";
+        
+        can.fillStyle = "white";
         can.font = "bold 16px Arial";
-        can.fillText(listElements[i].name, x, maxHeight + 16);
+        can.fillText(listElements[i].name, x-10, maxHeight + 20);
         x = x + spacing + rectWidth ;
     }
 }
@@ -62,4 +71,23 @@ class Element {
         this.value = value;
     }
 }
-    
+    // ###############
+function navbarEvent() {
+    const body = document.querySelector("body");
+    const navbar = document.querySelector(".navbar");
+    const menuBtn = document.querySelector(".menu-btn");
+    const cancelBtn = document.querySelector(".cancel-btn");
+    menuBtn.onclick = ()=>{
+    navbar.classList.add("show");
+    menuBtn.classList.add("hide");
+    body.classList.add("disabled");
+    }
+    cancelBtn.onclick = ()=>{
+    body.classList.remove("disabled");
+    navbar.classList.remove("show");
+    menuBtn.classList.remove("hide");
+    }
+    window.onscroll = ()=>{
+    this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+    }
+}
